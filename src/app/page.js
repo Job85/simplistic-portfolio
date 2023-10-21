00d5d9c038c7b7761fd5c4a1e2aac70927ca4396
Job85 <joulescodinglab@gmail.com>
2023-10-21 01:03:07 -0400

    imported NavBar and rendered component in <main> tag

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
-import styles from './page.module.css'
+import styles from './page.module.css';
+import NavBar from './components/Nav/NavBar';
 
 export default function Home() {
   return (
     <main>
+      <NavBar />
       <h1 className={styles.homeH1}>Hello!</h1>
     </main>
   )
