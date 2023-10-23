8e424f98d83a051806a43c0519997e8f5cf38420
Job85 <joulescodinglab@gmail.com>
2023-10-23 00:28:36 -0400

    Removed NavBar component

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
 import styles from './page.module.css';
-import NavBar from './components/Nav/NavBar';
 
 export default function Home() {
   return (
     <main>
-      <NavBar />
       <h1 className={styles.homeH1}>Hello!</h1>
     </main>
   )
