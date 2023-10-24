4e78787bdfec3309a889c46b1e35b38d0c9cdab6
Job85 <joulescodinglab@gmail.com>
2023-10-23 22:57:25 -0400

    mistake

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,12 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <>
-      <h1 className={styles.homeH1}>
+      <h1 className={styles.h1}>
         Resolving Complex Problems <br />
         Through <span className={styles.bold}>Innovative</span> and <span className={styles.bold}>Efficient</span> Design
       </h1>
       <h2 className={styles.h2}>
-        Hi, I'm <span className={styles.bold}>Julian Jernigan</span>, a full-stack software engineer with a focus on <span className={styles.bold}>back-end</span> development
+        Hi, I'm <span className={styles.bold}>Julian Jernigan</span>, a <span className={styles.bold}>full-stack</span> software engineer with a focus on <span className={styles.bold}>back-end</span> development
       </h2>
     </>
   )
