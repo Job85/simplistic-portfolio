b39766b1587f80f680beeafa6b3790c5fc02dffc
Job85 <joulescodinglab@gmail.com>
2023-10-23 21:38:09 -0400

    added h1 with headline and h2 with brief bio

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,14 @@ import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <main>
-      <h1 className={styles.homeH1}>Hello!</h1>
-    </main>
+    <>
+      <h1 className={styles.homeH1}>
+        Resolving Complex Problems <br />
+        Through <span className={styles.bold}>Innovative</span> and <span className={styles.bold}>Efficient</span> Design
+      </h1>
+      <h2 className={styles.h2}>
+        Hi, I'm <span className={styles.bold}>Julian Jernigan</span>, a full-stack software engineer with a focus on <span className={styles.bold}>back-end</span> development
+      </h2>
+    </>
   )
 }
