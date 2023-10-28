c53027bb2b376e9679c3bfe528aa4b2b97e652fe
Job85 <joulescodinglab@gmail.com>
2023-10-27 22:23:21 -0400

    removed color from h1 & h2; changed color of container class -> #C0C0C0; created border animation for container class with before & after w/absolute position & container w/relative position and overflow hidden

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.container}>
       <h1 className={styles.h1}>
         Resolving Complex Problems <br />
         Through <span className={styles.bold}>Innovative</span> and <span className={styles.bold}>Efficient</span> Design
@@ -10,6 +10,6 @@ export default function Home() {
       <h2 className={styles.h2}>
         Hi, I'm <span className={styles.bold}>Julian Jernigan</span>, a <span className={styles.bold}>full-stack</span> software engineer with a focus on <span className={styles.bold}>back-end</span> development
       </h2>
-    </>
+    </div>
   )
 }
