1c7c16cfbaa45b3b59109189e1665a59d28179d7
Job85 <joulescodinglab@gmail.com>
2023-10-29 18:45:07 -0400

    created page with h1 with two paragraphs for who I am and what I do; h2 for skills section

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,18 +2,25 @@ import styles from './page.module.css'
 
 export default function About() {
     return (
-        <div className={styles.container}>
-            <h1 className={styles.h1}>
-                About Me
-            </h1>
-            <p className={styles.para1}>
-                Hello, I'm Julian Jernigan, and I've embarked on a tech-driven quest &#128640; to reshape the digital landscape through imaginative solutions as a full-stack software engineer.
-                My conviction is that a well-crafted back-end forms the very backbone of every exceptional application, while user experience thrives with an appealing front-end.
-            </p>
-            <p className={styles.para2}>
-                My journey began with a deep dive into the world of software engineering, investing more than 480 hours in an immersive boot camp.
-                Through this intensive experience, I gained a profound understanding of JavaScript and honed my expertise in both the PERN &#40;PostgreSQL, Express, React, Node.js&#41; and MERN &#40;MongoDB, Express, React, Node.js&#41; stacks.
-            </p>
-        </div>
+        <>
+            <div className={styles.container}>
+                <h1 className={styles.h1}>
+                    About Me
+                </h1>
+                <p className={styles.para1}>
+                    Hello, I'm Julian Jernigan, and I've embarked on a tech-driven quest &#128640; to reshape the digital landscape through imaginative solutions as a full-stack software engineer.
+                    My conviction is that a well-crafted back-end forms the very backbone of every exceptional application, while user experience thrives with an appealing front-end.
+                </p>
+                <p className={styles.para2}>
+                    My journey began with a deep dive into the world of software engineering, investing more than 480 hours in an immersive boot camp.
+                    Through this intensive experience, I gained a profound understanding of JavaScript and honed my expertise in both the PERN &#40;PostgreSQL, Express, React, Node.js&#41; and MERN &#40;MongoDB, Express, React, Node.js&#41; stacks.
+                </p>
+            </div>
+            <div>
+                <h2 className={styles.h2}>
+                    Skills
+                </h2>
+            </div>
+        </>
     )
 }
