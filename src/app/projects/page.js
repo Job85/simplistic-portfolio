4fa2cf8717061365ada1ed2f1e5ff6f8d2faf3f8
Job85 <joulescodinglab@gmail.com>
2023-11-03 23:02:46 -0400

    created page to display projects inside class container with h1 tag inside

---
+++ src/app/projects/page.js
@@ -0,0 +1,9 @@
+import styles from "./page.module.css"
+
+export default function Projects() {
+    return (
+        <div className={styles.container}>
+            <h1>My Projects</h1>
+        </div>
+    )
+}
