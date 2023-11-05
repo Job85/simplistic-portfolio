0134a379bef60a6a9708be05f850017b1034ca55
Job85 <joulescodinglab@gmail.com>
2023-11-05 15:23:09 -0500

    imported Button component gave name 'Get in Touch'

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,21 @@
 import styles from './page.module.css';
+import Button from './components/Button/Button';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.h1}>
-        Resolving Complex Problems <br />
-        Through <span className={styles.bold}>Innovative</span> and <span className={styles.bold}>Efficient</span> Design
-      </h1>
-      <h2 className={styles.h2}>
-        Hi, I'm <span className={styles.bold}>Julian Jernigan</span>, a <span className={styles.bold}>full-stack</span> software engineer with a focus on <span className={styles.bold}>back-end</span> development
-      </h2>
+    <div>
+      <div className={styles.container}>
+        <h1 className={styles.h1}>
+          Resolving Complex Problems <br />
+          Through <span className={styles.bold}>Innovative</span> and <span className={styles.bold}>Efficient</span> Design
+        </h1>
+        <h2 className={styles.h2}>
+          Hi, I'm <span className={styles.bold}>Julian Jernigan</span>, a <span className={styles.bold}>full-stack</span> software engineer with a focus on <span className={styles.bold}>back-end</span> development
+        </h2>
+      </div>
+      <Button
+        name="Get in Touch"
+      />
     </div>
   )
 }
