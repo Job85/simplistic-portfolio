3543fbaa294301a44ab5a4e9b6437ce2588384d2
Job85 <joulescodinglab@gmail.com>
2023-11-05 01:55:06 -0400

    created a button component with basic styling border-color: #00CCFF, border-style: solid

---
+++ src/app/components/Button/Button.jsx
@@ -0,0 +1,15 @@
+import styles from "./Button.module.css";
+
+const Button = ({ name, onClick }) => {
+
+    return (
+        <button
+            className={styles.button}
+            onClick={onClick}
+        >
+            {name}
+        </button>
+    )
+}
+
+export default Button;
