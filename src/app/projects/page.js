373b135504aa68e7b5c8a47f49be727a12a9992b
Job85 <joulescodinglab@gmail.com>
2023-11-08 13:14:42 -0500

    imported localFont; created variable myFont with src fonts/neontubes2.otf and set variable in h1

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,10 +1,13 @@
+import localFont from 'next/font/local';
 import ProjectCards from "../components/ProjectCards/ProjectCards"
 import styles from "./page.module.css"
+
+const myFont = localFont({ src: '../fonts/neontubes2.otf' });
 
 export default function Projects() {
     return (
         <div className={styles.container}>
-            <h1 className={styles.page_header}>
+            <h1 className={`${styles.page_header} ${myFont.className}`}>
                 My Projects
             </h1>
             <div className={styles.project_container}>
