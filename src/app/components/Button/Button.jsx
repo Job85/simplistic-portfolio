bd72a7f598b8d68740b84c1c258067ab79bd0112
Job85 <joulescodinglab@gmail.com>
2023-11-10 18:16:34 -0500

    passed children as prop to Button

--- src/app/components/Button/Button.jsx
+++ src/app/components/Button/Button.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import styles from "./Button.module.css";
 
-const Button = ({ name, onClick, style }) => {
+const Button = ({ children, name, onClick, style }) => {
 
     return (
         <button
             className={`${styles.button} ${style}`}
             onClick={onClick}
         >
+            {children}
             {name}
         </button>
     )
