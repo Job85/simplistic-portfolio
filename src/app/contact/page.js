f8c0834e29b872c42a0b3a25ff6390e8ed874af6
Job85 <joulescodinglab@gmail.com>
2023-11-27 18:49:51 -0500

    created contact page

---
+++ src/app/contact/page.js
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+import { EmailForm } from "../components/Email/Email";
+
+export default function Contact() {
+
+    return (
+        <div>
+            <EmailForm />
+        </div>
+    )
+}
