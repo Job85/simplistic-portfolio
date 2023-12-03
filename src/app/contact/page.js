f2895b00593c1bdf383cf815cdaaed1a40f42756
Job85 <joulescodinglab@gmail.com>
2023-12-02 19:38:01 -0500

    created a text bubble for answering machine

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,13 +1,22 @@
 import styles from "./page.module.css";
 import { EmailForm } from "../components/Email/Email";
 import { AnsweringMachine } from "../components/AnsweringMachine/AnsweringMachine";
+import TypeWriter from "../components/TypeWriter/TypeWriter";
 
 export default function Contact() {
 
+    const text = ["Please leave a message after the beep..."]
     return (
         <div>
-            <div>
-                <AnsweringMachine />
+            <div className={styles.message}>
+                <div className={styles.text_bubble}>
+                    <TypeWriter
+                        texts={text}
+                    />
+                </div>
+                <div className={styles.answering_machine}>
+                    <AnsweringMachine />
+                </div>
             </div>
             <EmailForm />
         </div>
