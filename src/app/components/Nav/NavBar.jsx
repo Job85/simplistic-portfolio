b2d5381cd8bad3c83580c96c7016f50150033ec9
Job85 <joulescodinglab@gmail.com>
2023-12-03 00:00:30 -0500

    created a links variable as an array to map over <a> and <Link> tags

--- src/app/components/Nav/NavBar.jsx
+++ src/app/components/Nav/NavBar.jsx
@@ -4,46 +4,45 @@ import React from "react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import styles from "./Nav.module.css";
+import path from "path";
 
 const NavBar = () => {
 
     const pathname = usePathname();
 
+    const links = [
+        { href: "/", label: "Home" },
+        { href: "/about", label: "About" },
+        { href: "/projects", label: "Projects" },
+        { href: "/contact", label: "Contact" },
+        { href: "https://github.com/Job85", label: "GitHub" },
+        { href: "https://www.linkedin.com/in/julian-jernigan813/", label: "LinkedIn" },
+    ]
+
     return (
         <nav className={styles.nav_container}>
             <ul className={styles.nav_links}>
-                <Link
-                    className={`${styles.nav_link} ${pathname === '/' ? styles.active_link : ''}`}
-                    href="/"
-                >
-                    Home
-                </Link>
-                <Link
-                    className={`${styles.nav_link} ${pathname === '/about' ? styles.active_link : ''}`}
-                    href="/about"
-                >
-                    About
-                </Link>
-                <Link
-                    className={`${styles.nav_link} ${pathname === '/projects' ? styles.active_link : ''}`}
-                    href="/projects"
-                >
-                    Projects
-                </Link>
-                <a
-                    className={styles.nav_link}
-                    href="https://github.com/Job85"
-                    target="_blank"
-                >
-                    GitHub
-                </a>
-                <a
-                    className={styles.nav_link}
-                    href="https://www.linkedin.com/in/julian-jernigan813/"
-                    target="_blank"
-                >
-                    LinkedIn
-                </a>
+                {links.map((link, index) => (
+                    <li key={index}>
+                        {link.href.startsWith("http") ? (
+                            <a
+                                className={`${styles.nav_link} ${pathname === link.href ? styles.active_link : ''}`}
+                                href={link.href}
+                                target="_blank"
+                            >
+                                {link.label}
+                            </a>
+                        ) : (
+                            <Link
+                                href={link.href}
+                                passHref
+                                className={`${styles.nav_link} ${pathname === link.href ? styles.active_link : ''}`}
+                            >
+                                {link.label}
+                            </Link>
+                        )}
+                    </li>
+                ))}
             </ul>
         </nav>
     )
